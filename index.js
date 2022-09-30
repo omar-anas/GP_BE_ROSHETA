00a512eecfa3bd8321d6a96e42afb579eb601046
omar-anas <65462442+omar-anas@users.noreply.github.com>
2022-10-01 01:02:21 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const helmet = require('helmet');
+require('dotenv').config();
+
+//utils
+const logger = require('./Utils/logger');
+
+
+//secure headers
+app.use(helmet());
+
+
+app.use(cors());
+
+
+
+//middleware for cookies
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+
+
+
+//routes
+
+
+
+
+//listen to port 5000
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, async () => {
+    try {
+        logger.info('DB Connected .........')
+        logger.info(`server running on ${port}`)
+    } catch (error) {
+        logger.error(error);
+        process.exit(1);
+    }
+});
