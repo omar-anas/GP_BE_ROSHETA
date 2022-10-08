922c34de03044b430cbe5cf6ccf3a89f5aa36178
omar-anas <65462442+omar-anas@users.noreply.github.com>
2022-10-08 05:46:17 +0200

    doctor cruds

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -3,6 +3,7 @@ const DoctorController = require('../controller/doctor.controller')
 const PatientController = require('../controller/patient.controller')
 
 router.get('/doctor/getAll',DoctorController.getAllDoctors);
+router.get('/doctor/get/:id',DoctorController.getDoctor);
 router.post('/doctor/add',DoctorController.addDoctor);
 router.patch('/doctor/edit/:id',DoctorController.editDoctor);
 
