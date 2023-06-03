fab72dd2da8e90bca40de458f84b522a3480c988
MohammedMoataz <imohammedmoataz@gmail.com>
2023-06-03 21:10:06 +0300

    finish patinet APIs

--- routes/patient.routes.js
+++ routes/patient.routes.js
@@ -3,6 +3,9 @@ const PatientController = require('../controller/patient.controller')
 const patinetMiddleware = require('../middlewares/patientAuth.middleware')
 
 router.post('/register', PatientController.addPatient)
+
+router.patch('/edit/:id', patinetMiddleware, PatientController.editPatient)
+
 router.post('/symptom/add', patinetMiddleware, PatientController.addSymptom)
 router.get('/symptom/getAll', patinetMiddleware, PatientController.getSymptoms)
 router.delete('/symptom/delete', patinetMiddleware, PatientController.deleteSymptom)
@@ -10,6 +13,5 @@ router.delete('/symptom/delete', patinetMiddleware, PatientController.deleteSymp
 router.get('/get/:id', PatientController.getPatient)
 router.get('/getDoctors', patinetMiddleware, PatientController.getDoctors)
 
-router.patch('/edit/:id', patinetMiddleware, PatientController.editPatient)
 
 module.exports = router
